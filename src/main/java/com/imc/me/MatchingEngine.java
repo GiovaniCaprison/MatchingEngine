cdcead923217515023d24c637dc443afeeb1b5a6
Louis Grennell <louis.grennell@mongodb.com>
2026-06-26 13:08:44 +0100

    adding empty main method to ME class

--- src/main/java/com/imc/me/MatchingEngine.java
+++ src/main/java/com/imc/me/MatchingEngine.java
@@ -1,1 +1,7 @@
 package com.imc.me;
+
+public final class MatchingEngine {
+  public static void main(String[] args) {
+    System.out.println("Yea... We are so, so, still not there yet bru ))");
+  }
+}
