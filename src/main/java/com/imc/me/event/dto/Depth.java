e21df7f2c39935c1e5e4e5d01d30b90010f3a677
Louis Grennell <louis.grennell@mongodb.com>
2026-06-26 23:09:45 +0100

    (improvement): defining nested Level record within Depth record DTO

--- src/main/java/com/imc/me/event/dto/Depth.java
+++ src/main/java/com/imc/me/event/dto/Depth.java
@@ -3,4 +3,6 @@ package com.imc.me.event.dto;
 import com.imc.me.domain.OrderSide;
 import java.util.List;
 
-public record Depth(OrderSide side, List<Long> depth) {}
+public record Depth(OrderSide side, List<Level> levels) {
+  public record Level(long price, long qty) {}
+}
