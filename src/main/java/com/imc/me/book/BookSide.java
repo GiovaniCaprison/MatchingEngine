16d5541a01524fd3920d4aa11938f5bc979a49a6
Louis Grennell <louis.grennell@mongodb.com>
2026-06-26 16:12:12 +0100

    (feat): implementing BookSide interface API

--- src/main/java/com/imc/me/book/BookSide.java
+++ src/main/java/com/imc/me/book/BookSide.java
@@ -1,3 +1,21 @@
 package com.imc.me.book;
 
-public sealed interface BookSide permits TreeMapBookSide {}
+import com.imc.me.domain.Order;
+import com.imc.me.domain.OrderSide;
+import java.util.List;
+
+public sealed interface BookSide permits TreeMapBookSide {
+  boolean isEmpty();
+
+  OrderSide side();
+
+  PriceLevel bestLevel();
+
+  PriceLevel levelAt(long price);
+
+  List<PriceLevel> depth();
+
+  void addOrder(Order order);
+
+  void removeLevelIfEmpty(long price);
+}
