223806511faa1ffeb366178a6b517913432826b9
Louis Grennell <louis.grennell@mongodb.com>
2026-06-26 14:02:01 +0100

    (chore): removing OrderStatus enum and Java doc for OrderSide

--- src/main/java/com/imc/me/domain/OrderSide.java
+++ src/main/java/com/imc/me/domain/OrderSide.java
@@ -1,13 +1,5 @@
 package com.imc.me.domain;
 
-/**
- * The side of the order. Clients can either buy or sell.
- *
- * <ul>
- *   <li>Buy order - client wishes to buy (long) the instrument represented by this order
- *   <li>Sell order - client wishes to sell (short) the instrument represented by this order
- * </ul>
- */
 public enum OrderSide {
   BUY,
   SELL
